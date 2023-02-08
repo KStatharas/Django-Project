7715270325266f7b80a95639abda35caf6e416fa
Mutantin <39411418+Mutantin@users.noreply.github.com>
2023-02-08 02:48:27 +0200

    Django

---
+++ notes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('list', views.NotesListView.as_view(), name="notes.list"),
+    path('detail/<int:pk>',views.NoteDetailView.as_view(), name="notes.detail"),
+    path('edit/<int:pk>',views.NotesUpdateView.as_view(), name="notes.update"),
+    path('delete/<int:pk>',views.NotesDeleteView.as_view(), name="notes.delete"),
+    path('new',views.NotesCreateView.as_view(), name="notes.new"),
+]
